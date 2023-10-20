c025fdf4039ac4203af8bcb2b614891d241c5d4f
Minhhmmoi <127021373+Minhhmmoi@users.noreply.github.com>
2023-10-20 13:42:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "wo ai ni",
+    introTitle: 'Hello Dung nhé',
+    introDesc: `Anh có điều này muốn hỏi, em nhớ trả lời thật lòng nha :3`,
+    btnIntro: '^^Dạ^^',
+    title: 'Hôm nay là ngày 20/10 anh không có quà cho em ',
+    desc: ' em có thất vọng không ',
+    btnYes: 'có hơi buồn 1 chút!',
+    btnNo: 'Không nha :3',
+    question: 'Xin Lỗi em vì 20/10 này không thể tặng em 1 món quà thật ý nghĩa ',
+    btnReply: 'Để dịp khác anh bù đắp lại nhé',
+    reply: 'không sao anh ạ. có anh là được rồi. em yêu anh nhiều lắm',
+    mess: 'Anh biết mà 🥰. Anh cũng vậy đó 😘😘',
+    messDesc: 'Nghe nói hoa chỉ đẹp khi ở cạnh 1 đóa hoa đẹp. Chúc em một ngày 20/10 vui vẻ và thật xinh đẹp nhé',
+    btnAccept: 'Dạ',
+    messLink: 'https://minhhmmoi.github.io/dunggg/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
